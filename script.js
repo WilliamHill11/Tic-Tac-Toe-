765b7e2709d88636711ff90e0d778ab0196796b0
william hill <williamhill0831@Hotmail.com>
2023-05-16 15:39:04 -0400

    Working on building the render content for array

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 // player object
 
+let log = console.log;
+
 // gameBoard object
+const gameBoard = (function () {
+  let displayBoard = [];
+
+  //cache DOM
+  const cells = document.querySelectorAll('.cell');
+  const dataAttribute = document.querySelectorAll(['data-index']);
+  log(dataAttribute);
+
+  //render
+  function render() {
+    displayBoard.push(cells);
+    cellOne = displayBoard[0][0];
+    cellOne.textContent = 'o';
+    log(displayBoard);
+  }
+
+  //bind events
+  // cells.forEach((button) => {
+  //   button.addEventListener('click', markBoard);
+  // });
+
+  // function markBoard(e) {
+  //   cells.indexOf(e.dataset.index);
+  // }
+
+  return {
+    cells,
+    displayBoard,
+    render,
+  };
+})();
+
+gameBoard.render();
 
 // render function
 
-const cells = document.querySelectorAll('.cell');
+// gameBoard.cells.forEach((e) => {
+//   console.log(e.dataset.index);
+// });
 
-cells.forEach((e) => {
-  console.log(e.dataset.index);
-});
+// console.log(gameBoard.displayBoard);
 
 // restart button
