424b8e6e5c33a92b97cea67680d006cfc042f957
william hill <williamhill0831@Hotmail.com>
2023-05-16 12:47:36 -0400

    Built gameboard

---
+++ script.js
@@ -0,0 +1,13 @@
+// player object
+
+// gameBoard object
+
+// render function
+
+const cells = document.querySelectorAll('.cell');
+
+cells.forEach((e) => {
+  console.log(e.dataset.index);
+});
+
+// restart button
